3ecd746d693e7e8bff5275dbb9585407b200bd8a
scottish-james <82276449+scottish-james@users.noreply.github.com>
2025-01-17 22:36:37 +0000

    Create customer_landing.sql

---
+++ customer_landing.sql
@@ -0,0 +1,15 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `stedi`.`customer_landing` (
+  `customerName` string,
+  `email` string,
+  `phone` string,
+  `birthDay` string,
+  `serialNumber` string,
+  `registrationDate` bigint,
+  `shareWithResearchAsOfDate` bigint,
+  `shareWithPublicAsOfDate` bigint,
+  `shareWithFriendsAsOfDate` bigint
+)
+ROW FORMAT SERDE 'org.apache.hadoop.hive.ql.io.parquet.serde.ParquetHiveSerDe'
+STORED AS INPUTFORMAT 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.parquet.MapredParquetOutputFormat'
+LOCATION 's3://my-stedi-bucket-123456/customer/landing/'
+TBLPROPERTIES ('classification' = 'parquet');
