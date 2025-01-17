b90a421f3791acbfeee77c0f26c9b64944adab66
scottish-james <82276449+scottish-james@users.noreply.github.com>
2025-01-17 21:17:15 +0000

    Create accelerometer_landing.sql

---
+++ accelerometer_landing.sql
@@ -0,0 +1,16 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS `stedi`.`accelerometer_landing_2` (
+  `timestamp` bigint,
+  `x` float,
+  `y` float,
+  `z` float
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'FALSE',
+  'dots.in.keys' = 'FALSE',
+  'case.insensitive' = 'TRUE',
+  'mapping' = 'TRUE'
+)
+STORED AS INPUTFORMAT 'org.apache.hadoop.mapred.TextInputFormat' OUTPUTFORMAT 'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION 's3://my-stedi-bucket-123456/accelerometer/landing/'
+TBLPROPERTIES ('classification' = 'json');
